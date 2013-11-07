4c50bca4fff9238d22d0cb543327f2ce0393dee4
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2013-11-07 03:10:46 +0100

    Added test cases for the BufferPoolManager.

---
+++ minidbs-test/src/de/tuberlin/dima/minidb/test/io/AbstractTestBufferPoolManager.java
@@ -0,0 +1,12 @@
+package de.tuberlin.dima.minidb.test.io;
+
+import de.tuberlin.dima.minidb.io.BufferPoolManager;
+
+public class AbstractTestBufferPoolManager {
+
+	/**
+	 * The Buffer Pool Manager to be tested.
+	 */
+	protected BufferPoolManager underTest;
+	
+}
